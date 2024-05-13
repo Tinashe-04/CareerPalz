a2b539eeec025dd8432c87173b9dd9fd12d9ed1c
Tinashe-04 <maparuram@gmail.com>
2024-05-13 12:00:21 -0400

    Created python script to combine 2023 data into one file

---
+++ LCA2023.py
@@ -0,0 +1,11 @@
+""" 
+This script is used to create a single file for the LCA 2023 data.
+The required data is in 4 different files, one for each quarter of the year.
+I will use this script to combine the data from each of those four quarters into a single file.
+"""
+
+# import the libraries for data analysis
+import pandas as pd
+import os
+
+# Load the data
