598d03ba5de02ee679539ec551e7b5669673b023
Tinashe-04 <maparuram@gmail.com>
2024-05-13 12:27:21 -0400

    First trial of the code

--- LCA2023.py
+++ LCA2023.py
@@ -8,4 +8,22 @@ I will use this script to combine the data from each of those four quarters into
 import pandas as pd
 import os
 
-# Load the data
+# Set the path to the data files
+q1 = '/Users/tinashem/LCA Data/FY2023/LCA_Disclosure_Data_FY2023_Q1.xlsx'
+q2 = '/Users/tinashem/LCA Data/FY2023/LCA_Disclosure_Data_FY2023_Q2.xlsx'
+q3 = '/Users/tinashem/LCA Data/FY2023/LCA_Disclosure_Data_FY2023_Q3.xlsx'
+q4 = '/Users/tinashem/LCA Data/FY2023/LCA_Disclosure_Data_FY2023_Q4.xlsx'
+
+# Read the data and create a list of the dataframes
+data_q1 = pd.read_excel(q1)
+data_q2 = pd.read_excel(q2)
+data_q3 = pd.read_excel(q3)
+data_q4 = pd.read_excel(q4)
+
+df_list = [data_q1, data_q2, data_q3, data_q4]
+
+# find out how many rows and columns are in each of the dataframes
+print(df.shape for df in df_list)
+
+# Combine the data
+combined_data = pd.concat(df_list, ignore_index=True, verify_integrity=True)
